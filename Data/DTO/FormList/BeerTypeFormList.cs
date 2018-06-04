c57d60853e378dc4c44eea478a944e6ca3a7d4ca
Artur Czopek <artur.czopek@contractors.roche.com>
2018-06-04 02:56:03 +0200

    Add model BeerTypeFormList, BreweryFormList, CountryFormList

---
+++ Data/DTO/FormList/BeerTypeFormList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTO.FormList
+{
+    public class BeerTypeFormList {
+        public int value { get; set; }
+        public string text { get; set; }
+    }
+}
